27b09fd09fb56dd4fa5cc4fc731b95ee79b9faff
oba_kosuke_gn <oba_kosuke_gn@cyberagent.co.jp>
2013-06-28 18:55:40 +0900

    basic HTML

--- bbclock.js
+++ bbclock.js
@@ -1,4 +1,11 @@
 (function() {
+	(function() {
+		var c = $('#clock-anarog canvas')[0].getContext('2d');
+		c.beginPath();
+		c.arc(100, 100, 92, 0, Math.PI*2);
+		c.stroke();
+	})();
+
 	$(function() {
 		console.log('ready');
 	});
