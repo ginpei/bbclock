8dc5904440d8f90437062a8f6dbe1c6cf4a79915
oba_kosuke_gn <oba_kosuke_gn@cyberagent.co.jp>
2013-06-30 00:13:31 +0900

    split app and main

--- src/app.js
+++ src/app.js
@@ -1,35 +1,18 @@
-(function() {
-	// update anarog clock
-	(function() {
-		var c = $('#clock-anarog canvas')[0].getContext('2d');
-		c.beginPath();
-		c.arc(100, 100, 92, 0, Math.PI*2);
-		c.stroke();
-	})();
+/**
+ * Application instance.
+ */
+var app = {
+	start: function() {
+		var clock = new Clock();
+		var form = new TimeForm({
+			$el: $('#clock-form'),
+			model:clock
+		}).render();
+		var digital = new DigitalClockView({
+			$el: $('#clock-digital'),
+			model:clock
+		}).render();
 
-
-	/**
-	 * Application instance.
-	 */
-	var app = {
-		start: function() {
-			var clock = new Clock();
-			var form = new TimeForm({
-				$el: $('#clock-form'),
-				model:clock
-			}).render();
-			var digital = new DigitalClockView({
-				$el: $('#clock-digital'),
-				model:clock
-			}).render();
-
-			clock.startAuto();
-		}
-	};
-
-	// Let's Roll!
-	$(function() {
-		window.bbclock = app;
-		app.start();
-	});
-})();
+		clock.startAuto();
+	}
+};
